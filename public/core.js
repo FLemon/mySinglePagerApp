fcfde08ad0d76f2cf6c560b6b61b0bcbaaee6df7
FLemon <jin.xie@alliants.com>
2014-08-13 19:38:21 +0100

    use angular-route with routeProvider

--- public/core.js
+++ public/core.js
@@ -1,1 +1,8 @@
-angular.module('jinTodo', ['twitCtrl', 'twitService', 'todoCtrl', 'todoService']);
+angular.module('jinTodo', ['ngRoute', 'twitCtrl', 'twitService', 'todoCtrl', 'todoService']).
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+      controller: function() {},
+      templateUrl: '/templates/home.html'
+    })
+    .otherwise({redirectTo: '/'});
+  }]);
